247d3358674b7007ee968f15818e30dfbc33dba2
delicos <ali@web.de>
2016-10-02 10:56:03 +0200

    all new added

---
+++ MvcPresentation1/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPresentation1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Global field
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
